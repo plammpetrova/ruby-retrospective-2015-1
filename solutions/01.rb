e76d2bad618a26142efa65a2c11c6e2b8182f3a5
plammpetrova <plampetrova83@gmail.com>
2016-01-16 20:30:02 +0200

    My new solution to task #1

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,16 +1,8 @@
 def convert_to_bgn(price, currency)
-  rates_usd = {:usd => 1.7408}
-  rates_eur = {:eur => 1.9557}
-  rates_gbp = {:gbp => 2.6415}
-  if (currency.to_s <=> 'usd') == 0
-    (price*(rates_usd[:usd])).round(2)
-  elsif(currency.to_s <=> 'eur') == 0
-    (price*(rates_eur[:eur])).round(2)
-  else(currency.to_s <=> 'gbp') == 0
-    (price*(rates_gbp[:gbp])).round(2)
-  end
+  currencies = {usd: 1.7408, eur: 1.9557, gbp: 2.6415, bgn: 1.00}
+  (price * currencies[currency]).round(2)
 end
 
-def compare_prices(a, b, c, d)
-  convert_to_bgn(a, b) <=> convert_to_bgn(c, d)
+def compare_prices(price, currency, price_two, currency_two)
+  convert_to_bgn(price, currency) <=> convert_to_bgn(price_two, currency_two)
 end
